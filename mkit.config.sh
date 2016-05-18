b41a93c6837c8dc23be6c6fe9db5ac3043799584
Antonio Dell'Elce <antonio.dellelce@thomsonreuters.com>
2016-05-18 16:56:15 +0100

    moving configuration to an independent file

---
+++ mkit.config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# File:         mkit.config.sh
+# Created:      180516
+# Description:  description for mkit.config.sh
+#
+
+### FUNCTIONS ###
+
+### ENV ###
+
+ SRCLIST="sqlite3 m4 autoconf suhosin bison apr aprutil httpd openssl php pcre libxml2"
+ export srcget="0.0.5.5"  #  srcget version
+
+### EOF ###
