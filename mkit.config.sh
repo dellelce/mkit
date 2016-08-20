d67df7af6d86245b10993079a2dbccc46d1d8565
Antonio Dell'Elce <antouk@dellelce.com>
2016-08-20 15:16:34 +0100

    introducing readline / latet version of srcget: 0.0.6.1

--- mkit.config.sh
+++ mkit.config.sh
@@ -9,7 +9,7 @@
 
 ### ENV ###
 
- SRCLIST="sqlite3 m4 autoconf suhosin bison apr aprutil httpd openssl php pcre libxml2 zlib mod_wsgi python3"
- export srcget="0.0.6.0"  #  srcget version
+ SRCLIST="sqlite3 m4 autoconf suhosin bison apr aprutil httpd openssl php pcre libxml2 zlib mod_wsgi readline python3 "
+ export srcget="0.0.6.1"  #  srcget version
 
 ### EOF ###
