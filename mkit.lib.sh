00f572bf68c2a6fafeece5940818e1e531c534f9
Antonio Dell'Elce <antouk@dellelce.com>
2016-08-26 14:49:13 +0100

    getPerlVersion: needed for conditional download and install of perl

--- mkit.lib.sh
+++ mkit.lib.sh
@@ -18,6 +18,45 @@ popdir()
  return "$?"
 }
 
+# get perl versions as variables
+getPerlVersions()
+{
+ typeset perlBin="perl"
+
+ ${perlBin} -V | awk '
+FNR == 1 \
+{
+ gsub(/[()]/, " ");
+
+ cnt = split($0, a);
+ last = "" # state variable
+
+ for(idx in a)
+ {
+  item = a[idx]
+
+  if (item == "revision" || item == "version" || item == "subversion")
+  {
+   last = item
+   continue
+  }
+
+  if (last == "revision")   { revision = item;   last = ""; continue; }
+  if (last == "version")    { version = item;    last = ""; continue; }
+  if (last == "subversion") { subversion = item; last = ""; continue; }
+ }
+}
+
+END \
+{
+  printf("export PERL_REVISION=\"%s\";",   revision);
+  printf("export PERL_VERSION=\"%s\";",    version);
+  printf("export PERL_SUBVERSION=\"%s\";", subversion);
+}
+'
+#Summary of my perl5  revision 5 version 22 subversion 2  configuration:
+}
+
 # download srcget
 get_srcget()
 {
