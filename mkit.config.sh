e2c7f679dc88af94b69629d5dd0b1ff57e416371
Antonio Dell'Elce <antouk@dellelce.com>
2016-11-24 03:22:26 +0000

    ensure latest srcget

--- mkit.config.sh
+++ mkit.config.sh
@@ -11,6 +11,6 @@
 
  SRCLIST="sqlite3 m4 autoconf suhosin bison apr aprutil bzip2
              httpd openssl php pcre libxml2 zlib mod_wsgi readline python3 "
- export srcget="0.0.6.3"  #  srcget version
+ export srcget="0.0.6.5"  #  srcget version
 
 ### EOF ###
