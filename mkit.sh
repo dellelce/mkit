5c2b48a0bc2cc298d6c98a9651ce4107f693ec71
Antonio Dell'Elce <antouk@dellelce.com>
2018-07-13 12:21:25 +0100

    clean up comments (a bit) and remove hidden spaces

--- mkit.sh
+++ mkit.sh
@@ -12,7 +12,7 @@
 
  export srcgetUrl="https://github.com/dellelce/srcget/archive"
 
-# 
+#
 mkit_setup()
 {
  export TIMESTAMP="$(date +%H%M_%d%m%y)"
@@ -20,7 +20,7 @@ mkit_setup()
  export SRCGET="${WORKDIR}/srcget"
  export PATH="$PATH:$SRCGET"
  export DOWNLOADS="${WORKDIR}/downloads"
- 
+
  [ -z "$NO_TIMESTAMP" ] &&
  {
   export BUILDDIR="${WORKDIR}/build_${TIMESTAMP}"
@@ -30,9 +30,9 @@ mkit_setup()
   export BUILDDIR="${WORKDIR}/build"
   export SRCDIR="${WORKDIR}/src"
  }
- 
+
  export LOGSDIR="${WORKDIR}/logs"
-  
+
  ## "prefix" is the usual "GNU prefix" option i.e. the root of our install
  export prefix="${1:-$PWD}"
 
@@ -44,7 +44,7 @@ mkit_setup()
  }
 
  export PATH="$prefix/bin:$PATH"
- 
+
  mkdir -p "$BUILDDIR"
  mkdir -p "$LOGSDIR"
  mkdir -p "$SRCDIR"
@@ -56,21 +56,17 @@ mkit_setup()
  mkit_setup $*
  .  mkit.lib.sh
 
- echo
  echo "Install directory is ${prefix}"
- echo
 
  # download srcget
  get_srcget || { echo "Failed getting srcget, exiting..."; exit 1; }
 
- # the next function (download) uses the variable SRCLIST to determine 
- # which packages to download
  # TODO: check if perl is not installed at all?
-
+ #       this does not clarify which component requires perl > 5.10
  eval $(getPerlVersions)
 
  [ "$PERL_REVISION" -eq 5 -a "$PERL_VERSION" -lt 10 ] &&
- { 
+ {
   export SRCLIST="perl ${SRCLIST}"
   export PERL_NEEDED=1
   cat << EOF
@@ -80,6 +76,8 @@ mkit_setup()
 EOF
  }
 
+ # the next function (download) uses the variable SRCLIST to determine
+ # which packages to download
  # download latest archives / builds name mapping
  download || { echo "Download failed for one of the components"; exit 1; }
 
