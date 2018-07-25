b4f5352dffa6a6e0f81e1c4caefc22e5e28e142a
Antonio Dell'Elce <antouk@dellelce.com>
2018-07-25 17:19:48 +0100

    build_sanity_gnuconf: add more flexibility for missing configure

--- mkit.lib.sh
+++ mkit.lib.sh
@@ -230,11 +230,24 @@ build_sanity_gnuconf()
 {
  [ -z "$1" ] && { echo "build_sanity_gnuconf srcdirectory"; return 1; }
  [ ! -d "$1" ] && { echo "build_sanity_gnuconf: invalid srcdirectory: $1"; return 1; }
+ [ ! -f "$1/configure" -a -f "$1/configure.ac" -a -x "$prefix/bin/autoconf" ] &&
+ {
+  echo "build_sanity_gnuconf: no configure file in $1 but configure.ac is present and autoconf is available."
+  typeset cwd="$PWD"
+  typeset rc_autoconf=0
+  cd "$1"
+  mkdir -p m4  &&
+  $prefix/bin/autoheader &&
+  $prefix/bin/autoconf -i
+  rc_autoconf=$?
+  cd "$cwd"
+  return $rc_autoconf
+ }
  [ ! -f "$1/configure" -a -f "$1/buildconf.sh" ] &&
-  {
-   echo "build_sanity_gnuconf: no configure file in: $1 but buildconf.sh is present"
+ {
+  echo "build_sanity_gnuconf: no configure file in: $1 but buildconf.sh is present"
    return 2
-  }
+ }
  [ ! -f "$1/configure" ] && { echo "build_sanity_gnuconf: no configure file in: $1"; return 1; }
 
  return 0
