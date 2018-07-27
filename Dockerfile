68197d66a34319208876607dbddcef1d75cce895
Antonio Dell'Elce <antouk@dellelce.com>
2018-07-27 04:36:16 +0100

    Approaching success to docker build

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,14 @@ ENV BUILDDIR  /app-build
 ARG INSTALLDIR
 ENV INSTALLDIR  /app/httpd
 
+# gcc             most of the source needs gcc
+# bash            busybox does not support some needed features of bash like "typeset"
+# wget            builtin wget does not
+# perl            I'll pass...
+# file            no magic inside
+# xz              xz is the "best" 
+# libc-dev        headers
+# linux-headers   more headers
 ARG PACKAGES
 ENV PACKAGES gcc bash wget perl file xz make libc-dev linux-headers g++
 
@@ -16,3 +24,11 @@ COPY . $BUILDDIR
 
 RUN  apk add --no-cache  $PACKAGES &&  \
      bash ${BUILDDIR}/docker.sh $INSTALLDIR
+
+# Second Stage
+FROM alpine:latest AS final
+
+RUN mkdir -p /app/httpd
+
+WORKDIR /app/httpd
+COPY --from=build /app/httpd .
--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Building for docker
+# Building for docker // this is only needed until mkit does not have proper testing
 #
 # File:         docker.sh
 # Created:      250718
@@ -29,10 +29,56 @@ test_dir()
  return 0
 }
 
+main_tests()
+{
+ echo "Starting tests..."
+ test_dir  "$prefix/bin"
+ rc_bin=$?
+ [ "$rc_bin" -ne 0 ] && let fails="(( $fails + 1))"
+
+ fails=0
+
+ test_file $python
+ rc_python=$?
+
+ rc_sslversion=0
+
+ [ "$rc_python" -eq 0 ] &&
+ {
+  echo "Testing correct OpenSSL module is built:"
+  echo "import _ssl; print(_ssl.OPENSSL_VERSION);" | ${python}
+  rc_sslversion=$?
+ } ||
+ {
+  let fails="(( $fails + 1))"
+ }
+
+ [ "$rc_sslversion" -ne 0 ] && let fails="(( $fails + 1))"
+
+ # mod_wsgi checks
+ f="$prefix/modules/mod_wsgi.so"
+ test_file  $f || let fails="(( $fails + 1))"
+
+ f="$prefix/modules/mod_proxy_uwsgi.so"
+ test_file  $f || let fails="(( $fails + 1))"
+
+ echo
+ ls -lt "${prefix}/bin"
+
+ [ "$rc" -eq 0 -a "$fails" -ne 0 ] &&
+ {
+  echo "Build succeeded but there were $fails test failures!"
+  return 1
+ }
+
+ return $rc
+}
+
 ### ENV ###
 
 prefix="$1"
 python="$prefix/bin/python3.7"
+export fails=0
 
 ### MAIN ###
 
@@ -40,45 +86,9 @@ mkdir -p $prefix && ./mkit.sh $prefix
 rc=$?
 
 echo "mkit rc: $rc"
-fails=0
 
 # even if rc != 0: we do some tests anyway
+main_tests || exit $?
 
-test_dir  "$prefix/bin"
-rc_bin=$?
-[ "$rc_bin" -ne 0 ] && let fails="(( $fails + 1))" 
-
-test_file $python
-rc_python=$?
-
-rc_sslversion=0
-
-[ "$rc_python" -eq 0 ] &&
-{
- echo "Testing correct OpenSSL module is built:"
- echo "import _ssl; print(_ssl.OPENSSL_VERSION);" | ${python}
- rc_sslversion=$?
-} ||
-{
- let fails="(( $fails + 1))"
-}
-
-[ "$rc_sslversion" -ne 0 ] && let fails="(( $fails + 1))" 
-
-# mod_wsgi checks
-
-f="$prefix/modules/mod_wsgi.so"
-test_file  $f || let fails="(( $fails + 1))"
-
-f="$prefix/modules/mod_proxy_uwsgi.so"
-test_file  $f || let fails="(( $fails + 1))"
-
-#
-echo
-ls -lt "${prefix}/bin"
-
-[ "$rc" -eq 0 -a "$fails" -ne 0 ] && { echo "Build succeeded but there were $fails test failures!"; exit 1; }
-
-exit $rc
 
 ### EOF ###
