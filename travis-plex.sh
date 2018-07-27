83e0043b7275f614eabcd8a4f0387eed51584536
Antonio Dell'Elce <antouk@dellelce.com>
2018-07-27 10:02:10 +0100

    make travis happy

---
+++ travis-plex.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# File:         travis-plex.sh
+# Created:      270718
+#
+
+### ENV ###
+
+ isdocker="$1"; shift
+ path="$1"; shift
+
+### MAIN ###
+
+ set -x
+ [ "$isdocker" ] &&
+ { docker build -t mkitbuild . ; exit $?; } ||
+ { ./travis.sh "$path"; exit $?; }
+
+### EOF ###
