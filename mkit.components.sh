79dca4692d138cc778de84ed122a3681fcad4b34
Antonio Dell'Elce <antouk@dellelce.com>
2018-10-19 04:04:56 +0200

    why did I decide to fight the build of garguantan colossal compiler?

--- mkit.components.sh
+++ mkit.components.sh
@@ -756,7 +756,11 @@ build_gcc()
  MAKEINFO=: \
  build_gnuconf gcc $srcdir_gcc  --enable-languages=c \
                    --with-gmp=${prefix} --with-mpfr=${prefix} --with-mpc=${prefix} \
-                   --disable-multilib
+                   --disable-multilib \
+                   --disable-lto \
+                   --with-system-zlib \
+                   --disable-libstdcxx \
+                   --disable-nls
  return $?
 }
 
