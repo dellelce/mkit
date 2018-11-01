811205d3e8856580b79278485d103c9f852bde13
Antonio Dell'Elce <antouk@dellelce.com>
2018-11-01 14:50:45 +0100

    readline: a bit more help for readline build system

--- modules/build/readline.sh
+++ modules/build/readline.sh
@@ -8,12 +8,13 @@ build_readline()
    rlmk="$srcdir_readline/shlib/Makefile.in"
 
    ls -lt $rlmk
-   sed -i -e 's/SHLIB_LIBS = @SHLIB_LIBS@/SHLIB_LIBS = @SHLIB_LIBS@ -lncurses/' $rlmk
+   typeset ncurses="-L${prefix}/lib -lncurses"
+   sed -i -e "s/SHLIB_LIBS = @SHLIB_LIBS@/SHLIB_LIBS = @SHLIB_LIBS@ "${ncurses}"/" $rlmk
    ls -lt $rlmk
 
    # commenting until a proper option for debugging is added
    #echo "Debug: lib in install target"
-   #ls -lt "$prefix/lib"
+   ls -lt "$prefix/lib/*ncurs*"
  }
 
  build_gnuconf readline $srcdir_readline
