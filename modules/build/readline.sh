39795fcc725bb721c4e5f3b29730ea866a90ff2d
Antonio Dell'Elce <antouk@dellelce.com>
2018-11-02 11:53:33 +0100

    readline: "fix" for shlib Makefile.in

--- modules/build/readline.sh
+++ modules/build/readline.sh
@@ -8,13 +8,13 @@ build_readline()
    rlmk="$srcdir_readline/shlib/Makefile.in"
 
    ls -lt $rlmk
-   typeset ncurses="-L${prefix}/lib -lncurses"
-   sed -i -e "s/SHLIB_LIBS = @SHLIB_LIBS@/SHLIB_LIBS = @SHLIB_LIBS@ "${ncurses}"/" $rlmk
+   typeset ncurses="-L${prefix}\/lib -lncurses"
+   sed -i -e "s:SHLIB_LIBS = @SHLIB_LIBS@:SHLIB_LIBS = @SHLIB_LIBS@ ${ncurses}:" $rlmk
    ls -lt $rlmk
 
    # commenting until a proper option for debugging is added
    #echo "Debug: lib in install target"
-   ls -lt "$prefix/lib/*ncurs*"
+   ls -lt "$prefix/lib/"
  }
 
  build_gnuconf readline $srcdir_readline
