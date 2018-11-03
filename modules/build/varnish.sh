5ff20577dc8887c17c36e30478efbbff8899d189
Antonio Dell'Elce <antouk@dellelce.com>
2018-11-03 19:56:08 +0100

    no manpages please

--- modules/build/varnish.sh
+++ modules/build/varnish.sh
@@ -2,6 +2,15 @@ build_varnish()
 {
  [ -d "${prefix}/lib/pkgconfig" ] && export PKG_CONFIG_PATH="${prefix}/lib/pkgconfig"
 
+ # I would kindly ask not to build man pages
+ [ -f "$srcdir_varnish/man/Makefile" ] &&
+ {
+  echo 'install:' > "$srcdir_varnish/man/Makefile"
+ } ||
+ {
+  echo > "$srcdir_varnish/man/Makefile.am"
+ }
+
  RST2MAN=: SPHINX=: \
  build_gnuconf varnish $srcdir_varnish
  return $?
