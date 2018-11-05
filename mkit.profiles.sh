904e8519d066d05db51178362b1e2e3ea7068212
Antonio Dell'Elce <antouk@dellelce.com>
2018-11-05 16:50:25 +0100

    new profile for building gccgo

--- mkit.profiles.sh
+++ mkit.profiles.sh
@@ -128,6 +128,19 @@ profile_gcc7()
  return $?
 }
 
+profile_gccgo7()
+{
+ add_build_dep m4
+ add_run_dep zlib
+ add_run_dep binutils
+ add_run_dep gmp
+ add_run_dep mpfr
+ add_run_dep mpc
+ add_options gcc7 go
+ add_run_dep gcc7
+ return $?
+}
+
 profile_varnish()
 {
  # varnish needs python (">= 2.7") for generating some files
--- modules/build/gcc7.sh
+++ modules/build/gcc7.sh
@@ -1,8 +1,13 @@
 build_gcc7()
 {
+ typeset languages="c"
+ typeset args="$*"
+
+ [ ! -z "$args" ] && languages="${languages},${args}"
+
  MAKEINFO=: \
  build_gnuconf gcc7 $srcdir_gcc7 \
-                   --enable-languages=c \
+                   --enable-languages=${languages} \
                    --with-gmp=${prefix} \
                    --with-mpfr=${prefix} \
                    --with-mpc=${prefix} \
