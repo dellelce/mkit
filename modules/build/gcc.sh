a55527988cf4ddcd1d60e499c1c7713abbb42300
Antonio Dell'Elce <antouk@dellelce.com>
2018-11-06 14:15:14 +0100

    gcc: alpine options

--- modules/build/gcc.sh
+++ modules/build/gcc.sh
@@ -1,8 +1,11 @@
+# TODO: support cross-compiling
+# TODO: c++ is pushed in into languages, why?
 build_gcc()
 {
  typeset languages="c"
  typeset args="$*"
  typeset rc=0
+ typeset extra_args=""
 
  [ ! -z "$args" ] && languages="${languages},${args}"
 
@@ -15,6 +18,12 @@ build_gcc()
    export LD_LIBRARY_PATH="$prefix/lib:$LD_LIBRARY_PATH"
  }
 
+ [ -f "/etc/alpine-release" ] &&
+ {
+   extra_args="--disable-libssp --disable-libmpx --disable-libmudflap --disable-libsanitizer"
+   extra_args="${extra_args} --disable-gomp --disable-libatomic"
+ }
+
  MAKEINFO=: \
  build_gnuconf gcc $srcdir_gcc \
                    --enable-languages=${languages} \
@@ -24,7 +33,8 @@ build_gcc()
                    --disable-multilib \
                    --disable-lto \
                    --with-system-zlib \
-                   --disable-nls
+                   --disable-nls \
+                   ${extra_args}
  rc=$?
 
  [ ! -z "$OLD_LP" ] && { LD_LIBRARY_PATH="$OLD_LP"; unset OLD_LP; }
