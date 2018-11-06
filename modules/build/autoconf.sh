0ea847f852478022e880e988d2b07023e525e64b
Antonio Dell'Elce <antouk@dellelce.com>
2018-11-06 15:10:12 +0100

    autoconf: allow mixed "local" & in-build "aclocal"

--- modules/build/autoconf.sh
+++ modules/build/autoconf.sh
@@ -2,6 +2,16 @@
 #
 build_autoconf()
 {
+ typeset rc=0
+ # include pre-existing aclocal path if any
+ type aclocal >/dev/null 2>&1
+ rc=$?
+
+ [ "$rc" -eq 0 ]  &&
+ {
+   export ACLOCAL_PATH="${prefix}/share/aclocal:$(aclocal --print-ac-dir)"
+ }
+
  build_gnuconf autoconf $srcdir_autoconf
  return $?
 }
