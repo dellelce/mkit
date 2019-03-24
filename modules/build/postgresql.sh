cc407abca491f0e59f5629c872f473a2dce7d095
Antonio Dell'Elce <antouk@dellelce.com>
2019-03-24 13:59:32 +0000

    postgresql: "tap tests" not needed also backslash must be last character in line

--- modules/build/postgresql.sh
+++ modules/build/postgresql.sh
@@ -5,7 +5,6 @@ build_postgresql()
  [ -d "${prefix}/lib/pkgconfig" ] && export PKG_CONFIG_PATH="${prefix}/lib/pkgconfig"
  build_gnuconf postgresql $srcdir_postgresql  \
 			--disable-rpath       \
-			--enable-tap-tests    \  # uncertain of this, leaving as a note
 			--enable-thread-safety
  return $?
 }
