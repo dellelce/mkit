a1e7765102d145463f02561fa3f33c810f1ee69c
Antonio Dell'Elce <antouk@dellelce.com>
2019-03-28 15:04:42 +0100

    bind: do not use symtable

--- modules/build/bind.sh
+++ modules/build/bind.sh
@@ -5,6 +5,7 @@ build_bind()
 {
  ${prefix}/bin/pip3 install ply
  build_gnuconf bind $srcdir_bind  --with-openssl=${prefix} \
+				  --disable-symtable	   \
 				  --disable-linux-caps
  return $?
 }
