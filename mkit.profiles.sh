9b446ea52718d11f790707bbcc1b4d31eaf11aa5
Antonio Dell'Elce <antouk@dellelce.com>
2019-05-17 20:00:59 +0100

    timescaledb: correct profile name

--- mkit.profiles.sh
+++ mkit.profiles.sh
@@ -101,7 +101,7 @@ profile_timescaledb()
  add_build_dep cmake
  add_build_dep bison # only when building from commit/tag/branch (not "packaged" source)
  profile_postgres
- add_run_dep timescaledb10
+ add_run_dep timescaledb
  return $?
 }
 
