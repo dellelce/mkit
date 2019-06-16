beb76f23f0ca0b6072d1b84252017d3ca1f0fee7
Antonio Dell'Elce <antouk@dellelce.com>
2019-06-16 03:15:07 +0100

    opencsg: force in external flags

--- modules/build/opencsg.sh
+++ modules/build/opencsg.sh
@@ -3,6 +3,19 @@
 # opencsg
 build_opencsg()
 {
+ # Patch Makefile so it accesses dependencies in out paths.
+ sed -E -i 's/^CFLAGS(.*)/CFLAGS\1 $(CFLAGS)/' $srcdir_opencsg/src/Makefile
+ sed -E -i 's/^CXXFLAGS(.*)/CXXFLAGS\1 $(CXXFLAGS)/' $srcdir_opencsg/src/Makefile
+ sed -E -i 's/^LIBS(.*)/LIBS\1 $(LDLAGS)/' $srcdir_opencsg/src/Makefile
+
+ # sanity check: add "standard" flags if missing
+ [ -z "$CFLAGS" ] && export CFLAGS="-I${prefix}/include"
+ [ -z "$CXXFLAGS" ] && export CXXFLAGS="-I${prefix}/include"
+ [ -z "$LDFLAGS" ] && export LDFLAGS="-L${prefix}/lib -Wl,-rpath=${prefix}/lib"
+
+ CFLAGS="${CFLAGS}" \
+ CXXFLAGS="${CFLAGS}" \
+ LDFLAGS="${LDFLAFS}" \
  build_raw_core opencsg $srcdir_opencsg
 
  return $?
