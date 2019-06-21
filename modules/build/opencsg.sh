7b26c9edc396be2ad326fd251d8f92f44cc8a3f4
Antonio Dell'Elce <antouk@dellelce.com>
2019-06-21 13:03:30 +0100

    opencsg: use better names to avoid recursion

--- modules/build/opencsg.sh
+++ modules/build/opencsg.sh
@@ -4,18 +4,18 @@
 build_opencsg()
 {
  # Patch Makefile so it accesses dependencies in out paths.
- sed -E -i 's/^CFLAGS(.*)/CFLAGS\1 $(CFLAGS)/' $srcdir_opencsg/src/Makefile
- sed -E -i 's/^CXXFLAGS(.*)/CXXFLAGS\1 $(CXXFLAGS)/' $srcdir_opencsg/src/Makefile
- sed -E -i 's/^LIBS(.*)/LIBS\1 $(LDLAGS)/' $srcdir_opencsg/src/Makefile
+ sed -E -i 's/^CFLAGS(.*)/CFLAGS\1 $(_CFLAGS)/' $srcdir_opencsg/src/Makefile
+ sed -E -i 's/^CXXFLAGS(.*)/CXXFLAGS\1 $(_CXXFLAGS)/' $srcdir_opencsg/src/Makefile
+ sed -E -i 's/^LIBS(.*)/LIBS\1 $(_LDLAGS)/' $srcdir_opencsg/src/Makefile
 
  # sanity check: add "standard" flags if missing
  [ -z "$CFLAGS" ] && export CFLAGS="-I${prefix}/include"
  [ -z "$CXXFLAGS" ] && export CXXFLAGS="-I${prefix}/include"
  [ -z "$LDFLAGS" ] && export LDFLAGS="-L${prefix}/lib -Wl,-rpath=${prefix}/lib"
 
- CFLAGS="${CFLAGS}" \
- CXXFLAGS="${CFLAGS}" \
- LDFLAGS="${LDFLAFS}" \
+ _CFLAGS="${CFLAGS}" \
+ _CXXFLAGS="${CXXFLAGS}" \
+ _LDFLAGS="${LDFLAFS}" \
  build_raw_core opencsg $srcdir_opencsg
 
  return $?
