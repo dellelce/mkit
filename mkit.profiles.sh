5e2f74e2162ee1c2afcf29c68b1fb0d71ad1829f
Antonio Dell'Elce <antouk@dellelce.com>
2019-08-21 16:42:16 +0100

    new profile2: python2 quickie

--- mkit.profiles.sh
+++ mkit.profiles.sh
@@ -69,6 +69,15 @@ profile_pythonbuild()
  add_build_dep python3
 }
 
+#python2
+profile_python2()
+{
+ add_run_dep xz
+ add_run_dep libffi ncurses zlib bzip2 readline
+ add_run_dep openssl sqlite3 expat libxml2
+ add_run_dep python2
+}
+
 profile_uwsgi()
 {
  profile_python
