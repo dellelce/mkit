2743bcf5020eb5a2acfef0cc5758ffd5f40d4a21
Antonio Dell'Elce <antouk@dellelce.com>
2019-10-20 14:35:52 +0100

    make sure all versions of postgres are built with same parameters

--- modules/build/postgresql10.sh
+++ modules/build/postgresql10.sh
@@ -3,6 +3,10 @@
 build_postgresql10()
 {
  [ -d "${prefix}/lib/pkgconfig" ] && export PKG_CONFIG_PATH="${prefix}/lib/pkgconfig"
- build_gnuconf postgresql10 $srcdir_postgresql10
+ build_gnuconf postgresql10 $srcdir_postgresql10    \
+                            --disable-rpath         \
+                            --with-openssl          \
+                            --enable-thread-safety
+
  return $?
 }
--- modules/build/postgresql11.sh
+++ modules/build/postgresql11.sh
@@ -3,6 +3,11 @@
 build_postgresql11()
 {
  [ -d "${prefix}/lib/pkgconfig" ] && export PKG_CONFIG_PATH="${prefix}/lib/pkgconfig"
- build_gnuconf postgresql11 $srcdir_postgresql11
+ build_gnuconf postgresql11 $srcdir_postgresql11    \
+                            --disable-rpath         \
+                            --with-openssl          \
+                            --enable-thread-safety
+
+
  return $?
 }
