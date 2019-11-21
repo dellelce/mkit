fbf5e7f8d5b3d3882e1a0b81b2a8b7168b640d91
Antonio Dell'Elce <antouk@dellelce.com>
2019-11-21 18:05:09 +0000

    add "standalone" profile for X11

--- mkit.profiles.sh
+++ mkit.profiles.sh
@@ -382,4 +382,36 @@ profile_nettle()
  add_run_dep nettle
 }
 
+profile_x11()
+{
+ add_run_dep dri2proto
+ add_run_dep glproto
+ add_run_dep pciaccess
+ add_run_dep libdrm
+ add_run_dep xproto
+ add_run_dep xextproto
+ add_run_dep xtrans
+ add_run_dep kbproto
+ add_run_dep inputproto
+ add_run_dep xcbproto
+ add_run_dep pthreadstubs
+ add_run_dep xau
+ add_run_dep xcb
+ add_run_dep x11
+ add_run_dep damageproto
+ add_run_dep fixesproto
+ add_run_dep xfixes
+ add_run_dep xdamage
+ add_run_dep xext
+ add_run_dep xf86vidmodeproto
+ add_run_dep xxf86vm
+ add_run_dep xorgmacros
+ add_run_dep xshmfence
+ add_run_dep randrproto
+ add_run_dep renderproto
+ add_run_dep libxrender
+ add_run_dep libxrandr
+ add_run_dep xrandr
+}
+
 ### EOF ###
