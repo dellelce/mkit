f1f15d19663e3b9723b35db261e53552abfd8e2a
Antonio Dell'Elce <antouk@dellelce.com>
2020-04-04 19:08:46 +0100

    Revert "Build profile for "Proj"" (#30)
    * Revert "Build profile for "Proj" (#29)"
    
    This reverts commit f088e515e158a5df1cfa72510a2ebc9c7c532487.
    
    * add "proj" to travis

--- modules/libtiff/build.sh
+++
@@ -1,7 +0,0 @@
-build_libtiff()
-{
- [ -d "${prefix}/lib/pkgconfig" ] && export PKG_CONFIG_PATH="${prefix}/lib/pkgconfig";
-
- build_gnuconf libtiff $srcdir_libtiff 
- return $?
-}
--- modules/proj/build.sh
+++
@@ -1,7 +0,0 @@
-build_proj()
-{
- [ -d "${prefix}/lib/pkgconfig" ] && export PKG_CONFIG_PATH="${prefix}/lib/pkgconfig";
-
- build_gnuconf proj $srcdir_proj 
- return $?
-}
