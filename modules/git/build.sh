040f1045c7f87a51d862af3baa4412b638dd5a56
Antonio Dell'Elce <antouk@dellelce.com>
2021-06-15 16:39:44 +0100

    disable gettext if not available

--- modules/git/build.sh
+++ modules/git/build.sh
@@ -7,6 +7,10 @@ build_git()
 
  [ $? -eq 1 ] && { extra_opts="${etxra_opts} --without-tcltk"; }
 
+ type gettext >/dev/null 2>&1
+
+ [ $? -eq 1 ] && export NO_GETTEXT=1
+
  opt="BADCONFIGURE" \
  build_gnuconf git $srcdir_git \
           --with-zlib="${prefix}" ${extra_opts}
