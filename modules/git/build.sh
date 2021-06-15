3797efc6b3f8c8abe117a3a85d8926dcdef07740
Antonio Dell'Elce <antouk@dellelce.com>
2021-06-15 15:30:03 +0100

    disable tcltk build if tclsh not available

--- modules/git/build.sh
+++ modules/git/build.sh
@@ -1,8 +1,14 @@
 # git
 build_git()
 {
+ typeset extra_opts=""
+
+ type tclsh >/dev/null 2>&1
+
+ [ $? -eq 1 ] && { extra_opts="${etxra_opts} --without-tcltk"; }
+
  opt="BADCONFIGURE" \
  build_gnuconf git $srcdir_git \
-          --with-zlib="${prefix}"
+          --with-zlib="${prefix}" ${extra_opts}
  return $?
 }
