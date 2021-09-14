d5289cd5f3e98054186c4ed5a1638ec9e3f233e3
Antonio Dell'Elce <antouk@dellelce.com>
2021-09-14 13:18:39 +0100

    pass "cmake" style parameters to bootstrap wrapper as it is ignoring "prefix" argument to search for openssl

--- modules/cmake/build.sh
+++ modules/cmake/build.sh
@@ -1,5 +1,7 @@
+
+# cmake's configure is a wrapper to bootstrap that in turns uses cmake itself....
 build_cmake()
 {
- build_gnuconf cmake $srcdir_cmake
+ build_gnuconf cmake $srcdir_cmake -- -DOPENSSL_ROOT_DIR=${prefix}
  return $?
 }
