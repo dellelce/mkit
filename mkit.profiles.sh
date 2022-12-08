8bc2b98cf719b89a749e2c6c41f580742ae49568
Antonio Dell'Elce <antouk@dellelce.com>
2022-12-08 12:03:02 +0000

    temporarily remove libxml2 from python

--- mkit.profiles.sh
+++ mkit.profiles.sh
@@ -81,7 +81,7 @@ profile_python2()
 {
  add_run_dep xz
  add_run_dep libffi ncurses zlib bzip2 readline
- add_run_dep openssl sqlite3 expat libxml2
+ add_run_dep openssl sqlite3 expat
  add_run_dep python2
 }
 
