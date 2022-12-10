d62e2483fec6b5c04d0e150e9989ba7cce76825e
Antonio Dell'Elce <antouk@dellelce.com>
2022-12-10 13:07:02 +0000

    python: add back libxml2

--- mkit.profiles.sh
+++ mkit.profiles.sh
@@ -64,7 +64,7 @@ profile_python()
 {
  add_run_dep xz
  add_run_dep libffi ncurses zlib bzip2 readline
- add_run_dep openssl1_1 sqlite3
+ add_run_dep openssl1_1 sqlite3 expat libxml2
  add_run_dep python3
 }
 
@@ -81,7 +81,7 @@ profile_python2()
 {
  add_run_dep xz
  add_run_dep libffi ncurses zlib bzip2 readline
- add_run_dep openssl sqlite3 expat
+ add_run_dep openssl sqlite3 expat libxml2
  add_run_dep python2
 }
 
