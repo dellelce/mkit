29767a604fd5d25334022199c78795a46e013329
Antonio Dell'Elce <antouk@dellelce.com>
2022-12-25 13:29:48 +0000

    try harder to not build makeinfo stuff

--- modules/binutils/build.sh
+++ modules/binutils/build.sh
@@ -1,6 +1,12 @@
 #
 build_binutils()
 {
- build_gnuconf binutils $srcdir_binutils MAKEINFO=:
+ # try harder to skip makeinfo stuff
+ mkdir bin
+ echo ': $*' >bin/makeinfo
+ chmod +x bin/makeinfo
+ export PATH=$PWD/bin:$PATH
+
+ build_gnuconf binutils $srcdir_binutils
  return $?
 }
