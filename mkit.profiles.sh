81ecfbb9638e87420974feb86479f15952220079
Antonio Dell'Elce <antouk@dellelce.com>
2023-12-27 20:20:21 +0000

    update pythonbuild profile: add expat

--- mkit.profiles.sh
+++ mkit.profiles.sh
@@ -72,7 +72,7 @@ profile_pythonbuild()
 {
  add_build_dep xz
  add_build_dep libffi ncurses zlib bzip2 readline
- add_build_dep openssl1_1 sqlite3
+ add_build_dep openssl1_1 sqlite3 expat
  add_build_dep python3
 }
 
