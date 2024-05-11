4ad2d305971faab4aea859582670c4da7a3587a4
Antonio Dell'Elce <antouk@dellelce.com>
2024-05-11 12:38:53 +0100

    ncurses: ensure PKG_CONFIG_PATH is set

--- modules/ncurses/build.sh
+++ modules/ncurses/build.sh
@@ -3,6 +3,9 @@
 #
 build_ncurses()
 {
+ export PKG_CONFIG_PATH="${prefix}/lib/pkgconfig"
+ mkdir -p "$PKG_CONFIG_PATH"
+
  build_gnuconf ncurses $srcdir_ncurses --with-shared --with-cxx-shared  \
                                        --without-ada --disable-lib-suffixes
  return $?
