446f5a881590d88afb0e59b92d65ab693361ae75
Antonio Dell'Elce <antouk@dellelce.com>
2024-10-01 16:42:44 +0100

    postgresql: update openssl module name

--- mkit.profiles.sh
+++ mkit.profiles.sh
@@ -101,7 +101,7 @@ profile_postgres()
 # building with libressl fails with:
 #postgresql/postgresql-11.3/src/backend/libpq/be-secure-openssl.c:1103:63: error: ‘Port’ has no member named ‘peer'’
 #   strlcpy(ptr, X509_NAME_to_cstring(X509_get_subject_name(port->peer)), len);
- add_run_dep openssl
+ add_run_dep openssl1_1
  add_run_dep libxml2
  add_run_dep zlib
  add_run_dep ncurses
