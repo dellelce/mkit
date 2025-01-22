eb13fb41229c544a9ea186072fb66628df8f2267
Antonio Dell'Elce <antouk@dellelce.com>
2025-01-22 21:13:15 +0000

    ncurses: remove --disable-lib-suffixes

--- modules/ncurses/build.sh
+++ modules/ncurses/build.sh
@@ -6,8 +6,10 @@ build_ncurses()
  export PKG_CONFIG_PATH="${prefix}/lib/pkgconfig"
  mkdir -p "$PKG_CONFIG_PATH"
 
+ # --disable-lib-suffixes: if this is passed it won't generate ncursesw
+ #                         which is needed by readline at least with default options
  build_gnuconf ncurses $srcdir_ncurses --with-shared --with-cxx-shared  \
-                                       --without-ada --disable-lib-suffixes \
+                                       --without-ada \
                                        --enable-pc-files
  return $?
 }
